aa3ee0304a4ac066bdb8fa78136741c87e89fcc5
Sarthak Skills <kentojeeair1@gmail.com>
2026-06-18 08:05:34 +0000

    https://studio-5740165537-6af79-default-rtdb.asia-southeast1.firebasedat

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDummyKey",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "project.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "project",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "project.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "123456789",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:123456789:web:abcdef",
+  authDomain: "studio-5740165537-6af79.firebaseapp.com",
+  projectId: "studio-5740165537-6af79",
+  storageBucket: "studio-5740165537-6af79.firebasestorage.app",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef",
 };
