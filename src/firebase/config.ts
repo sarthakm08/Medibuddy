14d316dab1e8cf18459d1992f8eee28f669ba575
Sarthak Skills <kentojeeair1@gmail.com>
2026-06-18 08:07:13 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDSMeXlK4_ggRhELmVCtYO9SH9usVQL

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDummyKey",
+  apiKey: "AIzaSyDSMeXlK4_ggRhELmVCtYO9SH9usVQL3Es",
   authDomain: "studio-5740165537-6af79.firebaseapp.com",
+  databaseURL: "https://studio-5740165537-6af79-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "studio-5740165537-6af79",
   storageBucket: "studio-5740165537-6af79.firebasestorage.app",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef",
+  messagingSenderId: "189196146740",
+  appId: "1:189196146740:web:7c0377f9fbfa6a44c7d261"
 };
